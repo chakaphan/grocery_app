bbcd56543f87222314617314cb43fd7fce2f09ee
patipanc <patipanc@metis.co.th>
2026-06-01 21:26:15 +0700

    implement cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,17 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:grocery_app/screens/cart/cart_widget.dart';
+import 'package:grocery_app/services/utils.dart';
+import 'package:grocery_app/widgets/text_widget.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Color color = Utils(context).color;
+    Size size = Utils(context).getScreenSize;
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) {
-          return CartWidget();
-        },
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: TextWidget(
+          text: 'Cart (2)',
+          color: color,
+          textSize: 22,
+          isTitle: true,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(IconlyBroken.delete, color: color),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          _checkout(ctx: context),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (ctx, index) {
+                return CartWidget();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _checkout({required BuildContext ctx}) {
+    final Color color = Utils(ctx).color;
+    Size size = Utils(ctx).getScreenSize;
+
+    return SizedBox(
+      width: double.infinity,
+      height: size.height * 0.1,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Row(
+          children: [
+            Material(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(10),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(10),
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextWidget(
+                    text: 'Order Now',
+                    color: Colors.white,
+                    textSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+            FittedBox(
+              child: TextWidget(
+                text: 'Total: \$ 0.259',
+                color: color,
+                textSize: 18,
+                isTitle: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/cart/cart_widget.dart
+++ lib/screens/cart/cart_widget.dart
@@ -136,6 +136,7 @@ class _CartWidgetState extends State<CartWidget> {
                         ],
                       ),
                     ),
+                    const SizedBox(width: 5),
                   ],
                 ),
               ),
@@ -164,8 +165,8 @@ class _CartWidgetState extends State<CartWidget> {
               fct();
             },
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(icon, color: Colors.white),
+              padding: const EdgeInsets.all(6.0),
+              child: Icon(icon, color: Colors.white, size: 20),
             ),
           ),
         ),
